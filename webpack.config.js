354397c8857d23d441fbf24b5093c1b12bf7f261
franklin <franklinbenjamin032@gmail.com>
2021-12-17 21:09:09 -0400

    add dev env

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  devServer: {
+    static: './dist',
   },
   module: {
     rules: [
